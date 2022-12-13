76b91e4f52d93471246aced9f5a182f8a4364bda
Fede-Arevalo <arevalo.feder@gmail.com>
2022-12-13 14:26:08 +0100

    rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Redux
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/register" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register" element={<Login />} />
+          <Route path="/register" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
