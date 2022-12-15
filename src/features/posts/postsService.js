e1a7cb4492252c48330393369f0645c2a9240ffa
Fede-Arevalo <arevalo.feder@gmail.com>
2022-12-15 12:24:50 +0100

    postsService and postsSlice added

---
+++ src/features/posts/postsService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080";
+
+const getAllPosts = async () => {
+  const res = await axios.get(API_URL + "/posts/getAllPosts");
+  return res.data;
+};
+
+const postsService = {
+  getAllPosts,
+};
+
+export default postsService;
